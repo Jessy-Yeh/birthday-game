577f37900d33b21a9d7c8bdee228ddebbc08a2cb
Jessy Yeh <jiejun.yeh@gmail.com>
2023-10-31 16:14:10 +0000

    add material ui textfield

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -17,8 +17,82 @@ const questions = [
     month: "03",
   },
 ];
+
 const Game = () => {
-  return <>Game starts!</>;
+  const [questionCount, setQuestionCount] = useState(0);
+  const [year, setYear] = useState("");
+  const [month, setMonth] = useState("");
+
+  function submitForm(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+  }
+
+  const formCollections = questions.map((question, index) => {
+    return (
+      <div className={styles.form} key={index}>
+        <img className={styles.photo} src={question.link} />
+        <form onSubmit={submitForm}>
+          {/* <input
+              type="number"
+              placeholder="Year"
+              name="year"
+              value={year}
+              onChange={(e) => setYear(e.target.value)}
+              required
+            ></input>
+            <label htmlFor="year">請輸入照片年份</label> */}
+          <TextField
+            id="standard-basic"
+            type="number"
+            label="請輸入照片年份"
+            variant="standard"
+            sx={{
+              margin: "2em",
+              "& label.Mui-focused": {
+                color: "#ffec51",
+              },
+              "& .MuiInput-underline:after": {
+                borderBottomColor: "#ffec51",
+              },
+            }}
+            value={year}
+            onChange={(e) => setYear(e.target.value)}
+          />
+
+          {/* <p>請輸入照片月份</p> */}
+          {/* <input
+            value={month}
+            onChange={(e) => setMonth(e.target.value)}
+          ></input> */}
+          <TextField
+            id="standard-basic"
+            type="number"
+            label="請輸入照片月份"
+            variant="standard"
+            sx={{
+              margin: "2em",
+              "& label.Mui-focused": {
+                color: "#ffec51",
+              },
+              "& .MuiInput-underline:after": {
+                borderBottomColor: "#ffec51",
+              },
+            }}
+            value={month}
+            onChange={(e) => setMonth(e.target.value)}
+          />
+          <button
+            type="submit"
+            onClick={() => setQuestionCount((prev) => prev + 1)}
+          >
+            送出友情考卷
+          </button>
+        </form>
+      </div>
+    );
+  });
+
+  return <>{formCollections[questionCount]}</>;
 };
 
 export default Game;
