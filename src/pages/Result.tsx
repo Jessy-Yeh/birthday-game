f713af8162b113a33269dc9b61f8bb03d6a24224
Jessy Yeh <jiejun.yeh@gmail.com>
2023-11-02 10:09:10 +0000

    display correct number of answers based on different questions

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,41 +1,42 @@
 import styles from "./Game.module.css";
-import { Dispatch, SetStateAction } from "react";
-
-import photo1 from "../assets/2013-01-16.jpg";
-import photo2 from "/Users/yejiejun/Documents/web-dev/birthday-game/birthday-game/src/assets/2013-03-12.jpg";
+import { Question } from "./questions";
 
 interface Props {
-  year: string;
-  month: string;
-  clickSubmit: boolean;
-  setClickSubmit: Dispatch<SetStateAction<boolean>>;
-  questionCount: number;
-  setQuestionCount: Dispatch<SetStateAction<number>>;
+  answers: string[];
+  question: Question;
+  correctAnswerCount: number;
+  onClickNext: () => void;
 }
 
-const questions = [
-  {
-    link: photo1,
-    year: 2013,
-    month: 1,
-  },
-  {
-    link: photo2,
-    year: 2013,
-    month: 3,
-  },
-];
-
 const Result = ({
-  year,
-  month,
-  clickSubmit,
-  setClickSubmit,
-  questionCount,
-  setQuestionCount,
+  answers,
+  question,
+  correctAnswerCount,
+  onClickNext,
 }: Props) => {
-  const ifYearCorrect = Number(year) === questions[questionCount].year;
-  const ifMonthCorrect = Number(month) === questions[questionCount].month;
+  const resultMessages = [
+    <>
+      殘念 (ఠ్ఠ ˓̭ ఠ్ఠ) 您得到<b>0</b>友情分數 蓉雞倢蛙友情受到嚴酷考驗
+    </>,
+    <>
+      歪腰(//●⁰౪⁰●)// 您得到<b>{correctAnswerCount}</b>友情分數 蓉雞請繼續加油！
+    </>,
+    <>
+      漂亮٩(●˙▿˙●)۶…⋆ฺ 恭喜您得到<b>{correctAnswerCount}</b>友情分數
+      蓉雞太優秀了嗚呼～
+    </>,
+  ];
+  let message: JSX.Element = <></>;
+
+  if (correctAnswerCount === answers.length) {
+    message = resultMessages[2];
+  } else if (correctAnswerCount === 0) {
+    message = resultMessages[0];
+  } else {
+    message = resultMessages[1];
+  }
+
+  // const message = getMessage(countCorrectAnswers);
 
   return (
     <>
@@ -43,50 +44,36 @@ const Result = ({
         <div className={styles[`answer-section`]}>
           <div>
             <h2 className={styles[`answer-title`]}>蓉雞答案</h2>
-            <p className={ifYearCorrect ? "" : styles[`wrong-answer`]}>
-              {year}年
-            </p>
-            <p className={ifMonthCorrect ? "" : styles[`wrong-answer`]}>
-              {month}月
-            </p>
-          </div>
 
+            {answers.map((answer, index) => {
+              return (
+                <p
+                  key={index}
+                  className={
+                    answer === question.fields[index].solution
+                      ? ""
+                      : styles[`wrong-answer`]
+                  }
+                >
+                  {answer}
+                </p>
+              );
+            })}
+          </div>
           <div>
             <h2 className={styles[`answer-title`]}>正解</h2>
-            <p>{questions[questionCount].year}年</p>
-            <p>{questions[questionCount].month}月</p>
+            {question.fields.map((field, index) => (
+              <p key={index}>{field.solution}</p>
+            ))}
           </div>
         </div>
 
         <div>
-          {ifYearCorrect && ifMonthCorrect ? (
-            <p className={styles[`answer-message`]}>
-              漂亮٩(●˙▿˙●)۶…⋆ฺ 恭喜您得到<b>2</b>友情分數 蓉雞太優秀了嗚呼～
-            </p>
-          ) : null}
-
-          {(ifYearCorrect && !ifMonthCorrect) ||
-          (!ifYearCorrect && ifMonthCorrect) ? (
-            <p className={styles[`answer-message`]}>
-              歪腰(//●⁰౪⁰●)// 您得到<b>1</b>友情分數 蓉雞請繼續加油！
-            </p>
-          ) : null}
-
-          {!ifYearCorrect && !ifMonthCorrect ? (
-            <p className={styles[`answer-message`]}>
-              殘念 (ఠ్ఠ ˓̭ ఠ్ఠ) 您得到<b>0</b>友情分數 蓉雞倢蛙友情受到嚴酷考驗
-            </p>
-          ) : null}
+          <p className={styles[`answer-message`]}>{message}</p>
         </div>
       </div>
 
-      <button
-        type="submit"
-        onClick={() => {
-          setClickSubmit(!clickSubmit);
-          setQuestionCount((prev) => prev + 1);
-        }}
-      >
+      <button type="submit" onClick={onClickNext}>
         下一回合
       </button>
     </>
