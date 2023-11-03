20d9235f4404e1d287603b50a2b6546308cbe7f0
Jessy Yeh <jiejun.yeh@gmail.com>
2023-11-03 10:41:13 +0000

    update questions

--- src/pages/questions.ts
+++ src/pages/questions.ts
@@ -17,7 +17,11 @@ import photo16 from "../assets/2013-08-04 09.50.57.jpg";
 
 export type Question = {
   link: string;
-  fields: { label: string; solution: string }[];
+  fields: {
+    label: string;
+    solution: string;
+    checkAnswer?: (answer: string) => boolean;
+  }[];
 };
 
 export const questions: Question[] = [
@@ -40,10 +44,24 @@ export const questions: Question[] = [
       {
         label: "我們在幹嘛?",
         solution: "演戲",
+        checkAnswer: (answer) => {
+          if (answer.includes("演戲") || answer.includes("拍戲")) {
+            return true;
+          } else {
+            return false;
+          }
+        },
       },
       {
         label: "這個地點是?",
         solution: "中正宿舍陽台",
+        checkAnswer: (answer) => {
+          if (answer.includes("陽台") || answer.includes("宿舍")) {
+            return true;
+          } else {
+            return false;
+          }
+        },
       },
     ],
   },
@@ -53,7 +71,19 @@ export const questions: Question[] = [
   },
   {
     link: photo4,
-    fields: [{ label: "我們在哪唱歌?", solution: "納米" }],
+    fields: [
+      {
+        label: "我們在哪唱歌?",
+        solution: "納米",
+        checkAnswer: (answer) => {
+          if (answer.includes("納米")) {
+            return true;
+          } else {
+            return false;
+          }
+        },
+      },
+    ],
   },
   {
     link: photo5,
@@ -62,8 +92,28 @@ export const questions: Question[] = [
   {
     link: photo6,
     fields: [
-      { label: "這個地點是?", solution: "宿舍交誼廳" },
-      { label: "我們在幹嘛?", solution: "耍白痴？" },
+      {
+        label: "這個地點是?",
+        solution: "宿舍交誼廳",
+        checkAnswer: (answer) => {
+          if (answer.includes("交誼廳")) {
+            return true;
+          } else {
+            return false;
+          }
+        },
+      },
+      {
+        label: "我們在幹嘛?",
+        solution: "耍白痴？",
+        checkAnswer: (answer) => {
+          if (answer.includes("白痴") || answer.includes("笨")) {
+            return true;
+          } else {
+            return false;
+          }
+        },
+      },
     ],
   },
   {
@@ -74,12 +124,34 @@ export const questions: Question[] = [
     link: photo8,
     fields: [
       { label: "拍攝年份是?", solution: "2014" },
-      { label: "我們在幹嘛?", solution: "幫陳慕恩慶生？" },
+      {
+        label: "我們在幹嘛?",
+        solution: "幫陳慕恩慶生？",
+        checkAnswer: (answer) => {
+          if (answer.includes("慶生")) {
+            return true;
+          } else {
+            return false;
+          }
+        },
+      },
     ],
   },
   {
     link: photo9,
-    fields: [{ label: "誰比較白痴?", solution: "蓉雞" }],
+    fields: [
+      {
+        label: "誰比較白痴?",
+        solution: "蓉雞",
+        checkAnswer: (answer) => {
+          if (answer.includes("蓉") || answer.includes("我")) {
+            return true;
+          } else {
+            return false;
+          }
+        },
+      },
+    ],
   },
   {
     link: photo10,
@@ -90,30 +162,84 @@ export const questions: Question[] = [
   },
   {
     link: photo11,
-    fields: [{ label: "當天吃早餐遇到哪位蓉雞朋友?", solution: "韓籍" }],
+    fields: [
+      {
+        label: "當天吃早餐遇到哪位蓉雞朋友?",
+        solution: "韓籍",
+        checkAnswer: (answer) => {
+          if (answer.includes("韓")) {
+            return true;
+          } else {
+            return false;
+          }
+        },
+      },
+    ],
   },
   {
     link: photo16,
     fields: [
       { label: "倢蛙的身高多少?", solution: "164" },
-      { label: "那天在幹嘛?", solution: "迎新？" },
+      {
+        label: "那天在幹嘛?",
+        solution: "迎新？",
+        checkAnswer: (answer) => {
+          if (answer.includes("迎新") || answer.includes("新生")) {
+            return true;
+          } else {
+            return false;
+          }
+        },
+      },
     ],
   },
   {
     link: photo12,
-    fields: [{ label: "我們做了什麼活動?", solution: "射飛鏢" }],
+    fields: [
+      {
+        label: "我們做了什麼活動?",
+        solution: "射飛鏢",
+        checkAnswer: (answer) => {
+          if (answer.includes("飛鏢")) {
+            return true;
+          } else {
+            return false;
+          }
+        },
+      },
+    ],
   },
   {
     link: photo13,
     fields: [
       { label: "當天活動名稱?", solution: "白晝之夜" },
-      { label: "當天韓籍朋友叫什麼名字?", solution: "我忘了" },
+      {
+        label: "當天韓籍朋友叫什麼名字?",
+        solution: "我忘了",
+        checkAnswer: (answer) => {
+          if (answer.includes("忘")) {
+            return true;
+          } else {
+            return false;
+          }
+        },
+      },
     ],
   },
   {
     link: photo14,
     fields: [
-      { label: "這個地點是?", solution: "板橋耶誕城" },
+      {
+        label: "這個地點是?",
+        solution: "板橋耶誕城",
+        checkAnswer: (answer) => {
+          if (answer.includes("耶誕")) {
+            return true;
+          } else {
+            return false;
+          }
+        },
+      },
       { label: "來這裡的交通工具?", solution: "捷運" },
     ],
   },
@@ -121,7 +247,17 @@ export const questions: Question[] = [
     link: photo15,
     fields: [
       { label: "這座橋叫什麼?", solution: "數學橋" },
-      { label: "我們之中誰撐篙比較成功?", solution: "蓉雞" },
+      {
+        label: "我們之中誰撐篙比較成功?",
+        solution: "蓉雞",
+        checkAnswer: (answer) => {
+          if (answer.includes("蓉")) {
+            return true;
+          } else {
+            return false;
+          }
+        },
+      },
     ],
   },
 ];
