47b3c6079a39a35507f8e9b16cd8e8dbf0046d6f
Jessy Yeh <jiejun.yeh@gmail.com>
2023-11-03 11:31:12 +0000

    add confetti animation

--- src/pages/Thankyou/Thankyou.tsx
+++ src/pages/Thankyou/Thankyou.tsx
@@ -1,12 +1,18 @@
+import styles from "./Thankyou.module.css";
+import { useWindowSize } from "react-use";
+import Confetti from "react-confetti";
 interface Props {
   totalPoints: number;
 }
 
 const Thankyou = ({ totalPoints }: Props) => {
+  const { width, height } = useWindowSize();
+
   return (
     <>
-      <h1>蓉雞得到的友情分數是</h1>
-      <p>{totalPoints}分</p>
+      <Confetti width={width} height={height} />
+      <h1 className={styles.title}>蓉雞得到的友情分數是</h1>
+      <p className={styles.score}>{totalPoints}分</p>
     </>
   );
 };
