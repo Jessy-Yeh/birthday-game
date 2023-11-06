d4d4fa073ca8723d222863a7076be494dc79a3c3
Jessy Yeh <jiejun.yeh@gmail.com>
2023-11-06 11:38:30 +0000

    style homepage with rotating images

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,29 @@
 import { Link } from "react-router-dom";
+import styles from "./Home.module.css";
 
 const Home = () => {
   return (
-    <>
-      <h1>🐸倢蛙＆蓉雞🐥</h1>
-      <p className="subtitle">友情默契大考驗</p>
+    <div className={styles.background}>
+      <div className={styles[`title-group`]}>
+        <img
+          className={`${styles.frog} ${styles.rotating}`}
+          src="/public/frog.png"
+        />
+        <h1>倢蛙＆蓉雞</h1>
+        <img
+          className={`${styles.chick} ${styles.rotating}`}
+          src="/public/chick.png"
+        />
+      </div>
+      <p className={styles.subtitle}>友情默契大考驗</p>
       <Link to="/game">
         <button>開始</button>
       </Link>
-    </>
+      <img
+        className={styles[`friends-image`]}
+        src="../../../public/friends.jpg"
+      />
+    </div>
   );
 };
 
