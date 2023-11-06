0517125e723996cf3a1eb401706dd74c1878d0a2
Jessy Yeh <jiejun.yeh@gmail.com>
2023-11-06 12:11:24 +0000

    add timeout for Confetti

--- src/pages/Thankyou/Thankyou.tsx
+++ src/pages/Thankyou/Thankyou.tsx
@@ -1,5 +1,6 @@
 import styles from "./Thankyou.module.css";
 import { useWindowSize } from "react-use";
+import { useEffect, useState } from "react";
 import Confetti from "react-confetti";
 interface Props {
   totalPoints: number;
@@ -7,13 +8,22 @@ interface Props {
 
 const Thankyou = ({ totalPoints }: Props) => {
   const { width, height } = useWindowSize();
+  const [showConfetti, setShowConfetti] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowConfetti(false);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-    <>
-      <Confetti width={width} height={height} />
+    <div className={styles.page}>
+      {showConfetti && <Confetti width={width} height={height} />}
       <h1 className={styles.title}>蓉雞得到的友情分數是</h1>
       <p className={styles.score}>{totalPoints}分</p>
-    </>
+    </div>
   );
 };
 
