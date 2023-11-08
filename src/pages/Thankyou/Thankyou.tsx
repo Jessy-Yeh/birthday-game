19207d28ca8ea44b9246591dcd7f693add34431f
Jessy Yeh <jiejun.yeh@gmail.com>
2023-11-08 11:15:29 +0000

    enable scrolling to another section

--- src/pages/Thankyou/Thankyou.tsx
+++ src/pages/Thankyou/Thankyou.tsx
@@ -1,8 +1,15 @@
 import styles from "./Thankyou.module.css";
 import { useWindowSize } from "react-use";
-import { useEffect, useState } from "react";
+import {
+  useEffect,
+  useState,
+  useRef,
+  MutableRefObject,
+  MouseEventHandler,
+} from "react";
 import Confetti from "react-confetti";
-import Fade from "@mui/material/Fade";
+// import Fade from "@mui/material/Fade";
+import KeyboardDoubleArrowDownIcon from "@mui/icons-material/KeyboardDoubleArrowDown";
 interface Props {
   totalPoints: number;
 }
@@ -11,43 +18,87 @@ const Thankyou = ({ totalPoints }: Props) => {
   const { width, height } = useWindowSize();
   const [showConfetti, setShowConfetti] = useState(true);
   const [currentMessageIndex, setCurrentMessageIndex] = useState(0);
+  const [clickNext, setClickNext] = useState(false);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowConfetti(false);
-    }, 5000);
+  const messageRef = useRef(null);
+  console.log(messageRef);
 
-    return () => clearTimeout(timer);
-  }, []);
+  const scrollToSection = (elementRef) => {
+    window.scrollTo({
+      top: elementRef.current.offsetTop,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
 
-  const testMessages = [
-    <p>123</p>,
-    <p>hello mao</p>,
-    <p>this is Monday</p>,
-    <p>so</p>,
-    <p>all the best</p>,
-  ];
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     setShowConfetti(false);
+  //   }, 5000);
 
-  useEffect(() => {
-    const displayNextMessage = () => {
-      if (currentMessageIndex < testMessages.length - 1) {
-        setTimeout(() => {
-          setCurrentMessageIndex((prevIndex) => prevIndex + 1);
-        }, 3000);
-      }
-    };
+  //   return () => clearTimeout(timer);
+  // }, []);
 
-    displayNextMessage();
-  }, [currentMessageIndex]);
+  // const testMessages = [
+  //   <p>123</p>,
+  //   <p>hello mao</p>,
+  //   <p>this is Monday</p>,
+  //   <p>so</p>,
+  //   <p>all the best</p>,
+  // ];
+
+  // useEffect(() => {
+  //   const displayNextMessage = () => {
+  //     if (currentMessageIndex < testMessages.length - 1) {
+  //       setTimeout(() => {
+  //         setCurrentMessageIndex((prevIndex) => prevIndex + 1);
+  //       }, 3000);
+  //     }
+  //   };
+
+  //   displayNextMessage();
+  // }, [currentMessageIndex, testMessages.length]);
+
+  // function getMessage() {
+  //   setClickNext((prev) => !prev);
+
+  //   if (messageRef.current) {
+  //     messageRef.current.scrollIntoView({ behavior: "smooth" });
+  //   }
+  // }
 
   return (
     <div className={styles.page}>
-      {showConfetti && <Confetti width={width} height={height} />}
-      <h1 className={styles.title}>蓉雞得到的友情分數是</h1>
-      <p className={styles.score}>{totalPoints}分</p>
-      {testMessages[currentMessageIndex]}
+      <div className={styles[`first-section`]}>
+        <div></div>
+        <div>
+          {showConfetti && <Confetti width={width} height={height}></Confetti>}
+          <h1 className={styles.title}>蓉雞得到的默契分數是</h1>
+          <p className={styles.score}>{totalPoints * 4}分</p>
+        </div>
+
+        <div
+          role="button"
+          onClick={() => scrollToSection(messageRef)}
+          className={styles[`show-message`]}
+        >
+          <p>倢蛙給蓉雞的話</p>
+          <KeyboardDoubleArrowDownIcon></KeyboardDoubleArrowDownIcon>
+        </div>
+      </div>
+
+      {/* <button onClick={getMessage}>See Msg</button> */}
+
+      {/* {clickNext && testMessages[currentMessageIndex]} */}
+
+      {/* <div ref={messageRef}>
+          {clickNext && testMessages[currentMessageIndex]}
+        </div> */}
+
+      <div ref={messageRef} className={styles[`second-section`]}>
+        <h2>this is the second section wooo</h2>
+      </div>
     </div>
   );
 };
-
 export default Thankyou;
