4764604917817e1afd1762d6a9eb3f6facf7c3b4
Jessy Yeh <jiejun.yeh@gmail.com>
2023-11-08 17:50:26 +0000

    replace placeholder with correct text message

--- src/pages/Thankyou/Thankyou.tsx
+++ src/pages/Thankyou/Thankyou.tsx
@@ -40,14 +40,6 @@ const Thankyou = ({ totalPoints }: Props) => {
   //   return () => clearTimeout(timer);
   // }, []);
 
-  // const testMessages = [
-  //   <p>123</p>,
-  //   <p>hello mao</p>,
-  //   <p>this is Monday</p>,
-  //   <p>so</p>,
-  //   <p>all the best</p>,
-  // ];
-
   // useEffect(() => {
   //   const displayNextMessage = () => {
   //     if (currentMessageIndex < testMessages.length - 1) {
@@ -76,7 +68,8 @@ const Thankyou = ({ totalPoints }: Props) => {
           {/* {showConfetti && <Confetti width={width} height={height}></Confetti>} */}
           <Confetti width={width} height={height}></Confetti>
           <h1 className={styles.title}>蓉雞得到的默契分數是</h1>
-          <p className={styles.score}>{totalPoints * 4}分</p>
+          <p className={styles.score}>{totalPoints}分</p>
+          {/* <Marquee>生日快樂 生日快樂 生日快樂 生日快樂 生日快樂</Marquee> */}
         </div>
 
         <div
@@ -84,63 +77,64 @@ const Thankyou = ({ totalPoints }: Props) => {
           onClick={() => scrollToSection(messageRef)}
           className={styles[`show-message`]}
         >
-          <p>倢蛙給蓉雞的話</p>
+          <p>點我點我</p>
           <KeyboardDoubleArrowDownIcon></KeyboardDoubleArrowDownIcon>
         </div>
       </div>
 
-      {/* <button onClick={getMessage}>See Msg</button> */}
-
-      {/* {clickNext && testMessages[currentMessageIndex]} */}
-
-      {/* <div ref={messageRef}>
-          {clickNext && testMessages[currentMessageIndex]}
-        </div> */}
+      <div className={styles[`second-section`]}></div>
 
       <div ref={messageRef} className={styles[`second-section`]}>
         <h2>倢蛙給蓉雞の話</h2>
-        {/* <textarea
-          rows={25}
-          cols={35}
-          id="aboutDescription"
-          className={styles[`text-message`]}
-        > */}
-        sit amet metus. Proin nec condimentum risus, vel sollicitudin nisi.
-        Pellentesque dapibus magna id lobortis vestibulum. Duis porta eleifend
-        justo ut ultricies. Suspendisse potenti. Curabitur sapien velit,
-        tincidunt non tempus nec, molestie nec ligula. Morbi aliquet odio non
-        consequat malesuada. Phasellus in neque ut nisl rutrum hendrerit
-        consequat id lorem. In hac habitasse platea dictumst. Nunc sit amet
-        magna pellentesque, cursus elit non, pulvinar nisl. Sed in iaculis eros,
-        eget laoreet lorem. Mauris vestibulum elit ac mauris bibendum, eu
-        dapibus neque pulvinar. Fusce ultricies aliquet accumsan. Interdum et
-        malesuada fames ac ante ipsum primis in faucibus. Quisque vitae arcu ut
-        mi fermentum vulputate vel gravida nisl. Nunc in commodo sem.
-        Suspendisse potenti. Nam aliquet dignissim felis in cursus. Donec in
-        nunc efficitur, sodales nunc non, blandit mi. Proin porta metus felis,
-        eget aliquam ante semper in. Quisque lacinia mauris vel porta varius.
-        Phasellus at lorem feugiat, fermentum ex sed, iaculis neque. Cras
-        dignissim convallis felis sit amet gravida. Integer sit amet nisl
-        auctor, gravida dolor eget, suscipit magna. Donec porttitor erat et
-        venenatis ultrices. Maecenas tempus ultricies condimentum. Maecenas
-        pellentesque pharetra dui. Vivamus velit ante, finibus et augue vitae,
-        suscipit dictum erat. Mauris convallis accumsan sem, dapibus rutrum nisi
-        tincidunt sit amet. Integer sodales ligula quis varius rhoncus.
-        Suspendisse ut venenatis lectus. Mauris scelerisque dapibus convallis.
-        In in lacus quis lorem vestibulum ultricies eu in nisi. Aliquam erat
-        volutpat. Ma eget aliquam ante semper in. Quisque lacinia mauris vel
-        porta varius. Phasellus at lorem feugiat, fermentum ex sed, iaculis
-        neque. Cras dignissim convallis felis sit amet gravida. Integer sit amet
-        nisl auctor, gravida dolor eget, suscipit magna. Donec porttitor erat et
-        venenatis ultrices. Maecenas tempus ultricies condimentum. Maecenas
-        pellentesque pharetra dui. Vivamus velit ante, finibus et augue vitae,
-        suscipit dictum erat. Mauris convallis accumsan sem, dapibus rutrum nisi
-        tincidunt sit amet. Integer sodales ligula quis varius rhoncus.
-        Suspendisse ut venenatis lectus. Mauris scelerisque dapibus convallis.
-        In in lacus quis lorem vestibulum ultricies eu in nisi. Aliquam erat
-        volutpat. Ma
-        {/* </textarea> */}
-        <Marquee>生日快樂 生日快樂 生日快樂 生日快樂 生日快樂</Marquee>
+
+        <div className={styles[`text-message`]}>
+          <p>嘿，蓉雞！ 恭喜妳得到{totalPoints}默契分</p>
+          <p>其實不管有沒有滿分</p>
+          <p>我們的友誼早已超越所有分數！</p>
+          <p>希望妳有享受作答每一題哈哈</p>
+          <p>雖然想跟你說的部分的話</p>
+          <p>已經在上次我生日的時候說了</p>
+          <p>但是我還是想再次告訴妳</p>
+          <p>認識妳是我人生最幸運的事之一 </p>
+          <p>為了做這個小遊戲</p>
+          <p>我回頭翻了所有的老照片</p>
+          <p>每次看到都忍不住大笑</p>
+          <p>到底為什麼我們的怪表情這麼多 </p>
+          ...
+          <p>和妳認識已經11年</p>
+          <p>即便畢業後我們見面的次數也隨之減少</p>
+          <p>我們依然還是連繫著</p>
+          ...
+          <p>謝謝你沒有在我當時</p>
+          <p>一頭栽進愛情的時候離開我</p>
+          <p>總是聽我訴說各種喜怒哀樂 </p>
+          ...
+          <p>謝謝你去年跑來倫敦找我玩 </p>
+          <p>雖然妳剛好得Covid</p>
+          <p>但還是很堅強的走完全部行程 </p>
+          <p>我們又好似回到在中正住宿的那一年</p>
+          <p>這趟旅程</p>
+          <p>彷彿是我們遲來的畢業旅行</p>
+          <p>卻沒有因為遲來而減少半分喜悅</p>
+          <p>當時的我們</p>
+          <p>一定沒想到我們現在會在海外生活</p>
+          <p>謝謝我們一直為了自己的目標努力</p>
+          <p>就如同當初我們一個轉學一個轉系</p>
+          <p>即時如此</p>
+          <p>在我們嘗試找尋自己的道路上</p>
+          <p>我們之間的橋樑還是連接著</p>
+          ...
+          <p>我本來有點懼怕30歲的到來</p>
+          <p>後來我發現</p>
+          <p>這是一個很棒的年紀</p>
+          <p>是一個可以回頭檢閱自己</p>
+          <p>人生一路走來的時刻</p>
+          ...
+          <p>謝謝蓉雞的不離不棄</p>
+          <p>我們一起繼續在人生的道路上努力</p>
+          <p>蓉雞</p>
+          <p>生日快樂!!!!</p>
+        </div>
       </div>
     </div>
   );
