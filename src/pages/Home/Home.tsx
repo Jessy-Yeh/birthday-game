abd5b6e5e7d6c088c44de9d81f06a3f038c72022
Jessy Yeh <jiejun.yeh@gmail.com>
2023-11-09 18:01:52 +0000

    fix home page image src urls

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,24 +5,18 @@ const Home = () => {
   return (
     <div className={styles.background}>
       <div className={styles[`title-group`]}>
-        <img
-          className={`${styles.frog} ${styles.rotating}`}
-          src="/public/frog.png"
-        />
+        <img className={`${styles.frog} ${styles.rotating}`} src="/frog.png" />
         <h1>倢蛙＆蓉雞</h1>
         <img
           className={`${styles.chick} ${styles.rotating}`}
-          src="/public/chick.png"
+          src="/chick.png"
         />
       </div>
       <p className={styles.subtitle}>友情默契大考驗</p>
       <Link to="/game">
         <button>開始</button>
       </Link>
-      <img
-        className={styles[`friends-image`]}
-        src="../../../public/friends.jpg"
-      />
+      <img className={styles[`friends-image`]} src="/friends.jpg" />
     </div>
   );
 };
